6cffa680c6912264ddc998bd9a4276cc4ba569de
Pavan Yalamanchili <pavan@arrayfire.com>
2015-03-27 11:32:18 -0400

    Adding SCAN and REDUCE benchmarks for thrust

---
+++ src/thrust/reduce.cu
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/reduce.h>
+#include <valhalla.hpp>
+
+namespace vll {
+
+    template<typename T>
+    void reduce(const int num, bool gen = true, int iter = 0)
+    {
+        static thrust::device_vector<T> d_vec;
+
+        if (gen) {
+            thrust::host_vector<T> h_vec(num);
+            std::generate(h_vec.begin(), h_vec.end(), rand);
+            d_vec = h_vec;
+            cudaDeviceSynchronize();
+        }
+
+        for (int i = 0; i < iter; i++) {
+            T x = thrust::reduce(d_vec.begin(), d_vec.end());
+        }
+
+        cudaDeviceSynchronize();
+    }
+
+    template<typename T>
+    void generate(const int num)
+    {
+        try {
+            reduce<T>(num, true, 0);
+        } catch(...) {
+            // do nothing
+        }
+    }
+
+    template<typename T>
+    void run(const int iter)
+    {
+        try {
+            reduce<T>(-1, false, iter);
+        } catch(...) {
+            // do nothing
+        }
+    }
+}
+
+int main(int argc, const char **args)
+{
+    vll::launch(argc, args);
+}
---
+++ src/thrust/scan.cu
@@ -0,0 +1,54 @@
+#include <algorithm>
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/scan.h>
+#include <valhalla.hpp>
+
+namespace vll {
+
+    template<typename T>
+    void scan(const int num, bool gen = true, int iter = 0)
+    {
+        static thrust::device_vector<T> d_in;
+        static thrust::device_vector<T> d_out;
+
+        if (gen) {
+            thrust::host_vector<T> h_in(num);
+            std::generate(h_in.begin(), h_in.end(), rand);
+            d_in = h_in;
+            d_out = thrust::device_vector<T>(num);
+            cudaDeviceSynchronize();
+        }
+
+        for (int i = 0; i < iter; i++) {
+            thrust::inclusive_scan(d_in.begin(), d_in.end(), d_out.begin());
+        }
+
+        cudaDeviceSynchronize();
+    }
+
+    template<typename T>
+    void generate(const int num)
+    {
+        try {
+            scan<T>(num, true, 0);
+        } catch(...) {
+            // do nothing
+        }
+    }
+
+    template<typename T>
+    void run(const int iter)
+    {
+        try {
+            scan<T>(-1, false, iter);
+        } catch(...) {
+            // do nothing
+        }
+    }
+}
+
+int main(int argc, const char **args)
+{
+    vll::launch(argc, args);
+}

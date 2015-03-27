2961c237652d2c169398199674b0ece91d626d50
Pavan Yalamanchili <pavan@arrayfire.com>
2015-03-27 09:22:30 -0400

    Adding REDUCE and SCAN benchmark files for arrayfire

---
+++ src/arrayfire/scan.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <arrayfire.h>
+#include <af/traits.hpp>
+#include "../../include/common.hpp"
+
+
+namespace vll {
+
+    using namespace af;
+
+    array a;
+
+    template<typename T>
+    void generate(const int num)
+    {
+        a = randu(num, (af::dtype)dtype_traits<T>::af_type);
+    }
+
+    template<typename T>
+    void run(const int iter)
+    {
+        for (int i = 0; i < iter; i++) {
+            array b = accum(a);
+        }
+        af::sync();
+    }
+}
+
+int main(int argc, const char **args)
+{
+    vll::launch(argc, args);
+}

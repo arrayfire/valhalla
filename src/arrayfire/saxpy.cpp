caedeeccf6f819f1b84a0e6e7f3ac0a7561f0f93
Pavan Yalamanchili <pavan@arrayfire.com>
2015-03-30 16:12:28 -0400

    Adding SAXPY for arrayfire

---
+++ src/arrayfire/saxpy.cpp
@@ -0,0 +1,33 @@
+#include <arrayfire.h>
+#include <af/traits.hpp>
+#include <valhalla.hpp>
+
+namespace vll {
+
+    using namespace af;
+
+    float a;
+    array x, y;
+
+    template<typename T>
+    void generate(const int num)
+    {
+        a = 3.5;
+        x = randu(num, (af::dtype)dtype_traits<T>::af_type);
+        y = randu(num, (af::dtype)dtype_traits<T>::af_type);
+    }
+
+    template<typename T>
+    void run(const int iter)
+    {
+        for (int i = 0; i < iter; i++) {
+            y = a * x + y;
+            y.eval();
+        }
+    }
+}
+
+int main(int argc, const char **args)
+{
+    vll::launch(argc, args);
+}
